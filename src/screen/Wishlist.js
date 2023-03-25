0d78912106555652f8831a3575ae1b6fca74a395
rahulkottikasala <rahulwoxro@gmail.com>
2023-03-25 18:26:16 +0530

    wislist screen update

--- src/screen/Wishlist.js
+++ src/screen/Wishlist.js
@@ -1,14 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, useColorScheme } from 'react-native'
 import React from 'react'
 import Header from '../components/Header'
 import { ScrollView } from 'react-native-gesture-handler'
+import { Card } from '../components/product/Card'
+import ViewHeight from '../components/ViewHeight'
 
 const Wishlist = () => {
+  const isDark = useColorScheme() === "dark"
   return (
     <View style={styles.container}>
       <Header />
       <ScrollView showsVerticalScrollIndicator={false} >
-        
+        <Card isDark={isDark} />
+        <Card isDark={isDark} />
+        <ViewHeight/>
       </ScrollView>
      
     </View>
